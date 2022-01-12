536b11fcdfc97ae9826a9a3f5eb7df1b797eabea
GiovanniConti <giovanni.conti@live.it>
2022-01-12 19:25:39 +0100

    Created home page

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.default')
+
+{{-- Page Title --}}
+@section('page_title', 'Home')
+
+{{-- Main Content --}}
+@section('main_content')
+  
+  <div class="container">
+    
+  </div>
+
+@endsection
