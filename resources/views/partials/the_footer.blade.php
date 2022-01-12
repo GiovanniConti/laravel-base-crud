71924e7c64aeaef376dedef2539a20039cfa94e2
GiovanniConti <giovanni.conti@live.it>
2022-01-12 19:24:32 +0100

    Created default layout, header and footer partials

---
+++ resources/views/partials/the_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="text-center">
+  <h5 class="text-success py-3">Boolean Comic Store</h5>
+</footer>
---
+++ resources/views/partials/the_header.blade.php
@@ -0,0 +1,3 @@
+<header class="text-center">
+  <h1 class="text-primary py-5">Comic Store</h1>
+</header>
