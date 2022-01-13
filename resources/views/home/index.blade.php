8443c8ce110fefab61ad39024c821469e55b649d
GiovanniConti <giovanni.conti@live.it>
2022-01-13 17:56:50 +0100

    added Comics cards in homepage

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,7 +7,22 @@
 @section('main_content')
   
   <div class="container">
-    
+    <div class="row row-cols-1 row-cols-md-4 g-4">
+      @foreach ($comics as $comic)
+        <div class="col">
+          <div class="card">
+            <div class="card-img-container">
+              <img class="card-img-top" src="{{ $comic['thumb']}}" alt="">
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">{{ $comic['title']}}</h5>
+              <a href="">Dettagli</a>
+              <a href="">Modifica</a>
+            </div>
+          </div>
+        </div>
+      @endforeach
+    </div>
   </div>
 
 @endsection
